2584df78208b701c932199f233dc4614bc182cd9
lopesma11 <ml722090@gmail.com>
2024-12-14 12:43:25 -0300

    task: users controller and routing

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,29 @@ import express from "express";
 import { get, identity, merge } from "lodash";
 import { getUsersBySessionToken } from "db/users";
 
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  try {
+    const { id } = req.params;
+    const currentUserId = get(req, "identity_id") as string;
+
+    if (!currentUserId) {
+      return res.sendStatus(403);
+    }
+
+    if (currentUserId.toString() !== id) {
+      return res.sendStatus(403);
+    }
+    next();
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(400);
+  }
+};
+
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
