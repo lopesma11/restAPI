fac1c580de51d56db6723809ba1b926ccef70538
lopesma11 <ml722090@gmail.com>
2024-12-14 11:10:30 -0300

    task:authentication routing and logging in

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
+import router from "router";
 
 const app = express();
 
@@ -31,3 +32,5 @@ mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => {
   console.log(error);
 });
+
+app.use("/", router());
